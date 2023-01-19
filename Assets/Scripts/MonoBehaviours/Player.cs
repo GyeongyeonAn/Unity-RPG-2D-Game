0744b36f8a69c4d033be424dc1bbed51f7dbbbda
agy_97 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-01-19 20:49:57 +0900

    Heart 오브젝트 생성 - 플레이어와 충돌 시 체력 1 증가

--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,9 +9,27 @@ public class Player : Character
     {
         if (other.gameObject.CompareTag("CanBePickedUp"))
         {
-            other.gameObject.SetActive(false);
+            Item hitObject = other.gameObject.GetComponent<Consumable>().item;
+            if (hitObject != null)
+            {
+                switch (hitObject.itemType)
+                {
+                    case Item.ItemType.COIN:
+                        break;
+                    case Item.ItemType.HEALTH:
+                        AdjustHitPoints(hitObject.quantity);
+                        break;
+                    default:
+                        break;
+                }
+                other.gameObject.SetActive(false);
+            }
         }
+    }
 
-
+    public void AdjustHitPoints(int amount)
+    {
+        hitPoints += amount;
+        print("Adjust hit points by: " + amount + ". New value: " + hitPoints);
     }
 }
