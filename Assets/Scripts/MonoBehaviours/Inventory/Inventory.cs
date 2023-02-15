b2f973948e72f29126027741d940a7813a11773f
agy_97 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-02-15 15:36:13 +0900

    feat: 인벤토리 기능

---
+++ Assets/Scripts/MonoBehaviours/Inventory/Inventory.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject slotPrefab;
+    public const int numSlots = 5;
+    Image[] itemImages = new Image[numSlots];
+    Item[]
+}
